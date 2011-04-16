f9d7a6e55e7d7843fd66ac9efdedcdd663fc67a8
Kevin Wells <DarkOppressor@gmail.com>
2011-04-16 15:57:39 -0500

    Worked on item templates.

---
+++ item_template.cpp
@@ -0,0 +1,10 @@
+/* Copyright (c) 2011 Kevin Wells */
+/* Escape from the Master's Lair may be freely redistributed.  See license for details. */
+
+#include "item_template.h"
+
+using namespace std;
+
+Item_Template::Item_Template(){
+    size=0.0;
+}
---
+++ item_template.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2011 Kevin Wells */
+/* Escape from the Master's Lair may be freely redistributed.  See license for details. */
+
+#ifndef item_template_h
+#define item_template_h
+
+#include "item.h"
+
+class Item_Template: public Item{
+    private:
+    public:
+
+    //The size of the item.
+    double size;
+
+    //The materials this item can be.
+    //This is only used for items that do not have a color/material defined in their template entry.
+    std::vector<short> allowed_materials;
+
+    Item_Template();
+};
+
+#endif
