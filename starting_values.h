bb5e7f89045b7849638d7eb0cb045fdf61cfc8e6
Kevin Wells <DarkOppressor@gmail.com>
2011-04-19 15:38:22 -0500

    Restored a missing file. Oops!

--- starting_values.h
+++ starting_values.h
@@ -1,11 +1,13 @@
 /* Copyright (c) 2011 Kevin Wells */
 /* Escape from the Master's Lair may be freely redistributed.  See license for details. */
 
-#ifndef player_starting_values_h
-#define player_starting_values_h
+#ifndef starting_values_h
+#define starting_values_h
 
 const int STARTING_ITEMS_GOLD=25;
 
 const int STARTING_THROWN_STACK_SIZE=30;
+
+const int STARTING_SKILL_EXPERIENCE_MAX=200;
 
 #endif
