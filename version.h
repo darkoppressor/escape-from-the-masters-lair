b0e6187a41a8039502fc5af0ee846410773c32b4
Kevin Wells <DarkOppressor@gmail.com>
2012-08-04 02:00:07 -0500

    Updated libraries. Now in beta!

--- version.h
+++ version.h
@@ -4,26 +4,26 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "21";
-	static const char MONTH[] = "04";
-	static const char YEAR[] = "2011";
-	static const char UBUNTU_VERSION_STYLE[] = "11.04";
+	static const char DATE[] = "04";
+	static const char MONTH[] = "08";
+	static const char YEAR[] = "2012";
+	static const char UBUNTU_VERSION_STYLE[] = "12.08";
 	
 	//Software Status
-	static const char STATUS[] = "Alpha";
-	static const char STATUS_SHORT[] = "a";
+	static const char STATUS[] = "Beta";
+	static const char STATUS_SHORT[] = "b";
 	
 	//Standard Version Type
 	static const long MAJOR = 0;
-	static const long MINOR = 5;
+	static const long MINOR = 6;
 	static const long BUILD = 3117;
 	static const long REVISION = 17267;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 7394;
-	#define RC_FILEVERSION 0,5,3117,17267
-	#define RC_FILEVERSION_STRING "0, 5, 3117, 17267\0"
-	static const char FULLVERSION_STRING[] = "0.5.3117.17267";
+	#define RC_FILEVERSION 0,6,3117,17267
+	#define RC_FILEVERSION_STRING "0, 6, 3117, 17267\0"
+	static const char FULLVERSION_STRING[] = "0.6.3117.17267";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 57;
